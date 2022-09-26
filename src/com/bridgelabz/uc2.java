50e5775dfa7ce284362809b90e118dde0bbc8b95
Sujay509 <sujaychauhan98@gmail.com>
2022-09-26 07:33:09 +0530

    Added : Roll the Dice

---
+++ src/com/bridgelabz/uc2.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class uc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Random r = new Random();
+	       int die = r.nextInt(6);
+	       System.out.println("Number on dice : "+(die+1));
+	}
+
+}
